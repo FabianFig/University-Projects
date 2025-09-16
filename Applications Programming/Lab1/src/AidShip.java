0b6f4dbcfaa924723af4e22cc2a97014c8319aec
FabianFig <you@example.com>
2025-09-15 20:15:32 -0500

    Added javadocumentation to AidShip class

--- Applications Programming/Lab1/src/AidShip.java
+++ Applications Programming/Lab1/src/AidShip.java
@@ -1,7 +1,23 @@
+/**
+ * The AidShip class represents an AidShip
+ * @author Fabian Figueroa
+ */
+
 public class AidShip extends Ship implements EmergencySupport{
     private String aidType;
     private int suppliesOnBoard;
     private boolean hasHelipad;
+
+    /**
+     * @param name the name of the ship
+     * @param registrationNumber the registration number of the ship
+     * @param tonnage the weight of the ship
+     * @param crewSize the number of crew members aboard the ship
+     * @param currentPort the current location of the aid ship
+     * @param aidType the type of aid the ship is carrying
+     * @param suppliesOnBoard the amount of supplies the ship has on board
+     * @param hasHelipad whether the ship has a helipad or not
+     */
 
     public AidShip(String name, String registrationNumber, double tonnage, int crewSize,
                    String currentPort, String aidType, int suppliesOnBoard, boolean hasHelipad) {
@@ -11,35 +27,66 @@ public class AidShip extends Ship implements EmergencySupport{
         this.hasHelipad = hasHelipad;
     }
 
+    /**
+     * returns the type of aid aboard the shp
+     * @return String, the type of aid aboard
+     */
     public String getAidType() {
         return aidType;
     }
 
+    /**
+     * sets the type of aid
+     * @param aidType, the type of aid (String)
+     */
     public void setAidType(String aidType) {
         this.aidType = aidType;
     }
 
+    /**
+     * returns the number of supplies on board
+     * @return int, the number of supplies
+     */
     public int getSuppliesOnBoard() {
         return suppliesOnBoard;
     }
 
+    /**
+     * sets the amount of supplies on board
+     * @param suppliesOnBoard, the number of supplies on board (int)
+     */
     public void setSuppliesOnBoard(int suppliesOnBoard) {
         this.suppliesOnBoard = suppliesOnBoard;
     }
 
-    public boolean isHasHelipad() {
+    /**
+     * returns true or false depending on whether the ship has a helipad
+     * @return boolean, status of a helipad on board
+     */
+    public boolean HasHelipad() {
         return hasHelipad;
     }
 
+    /**
+     * sets the status of a helipad
+     * @param hasHelipad, status of a helippad on board (boolean)
+     */
     public void setHasHelipad(boolean hasHelipad) {
         this.hasHelipad = hasHelipad;
     }
 
+    /**
+     * prints string indicating the name of the ship and the number of aid it is deploying
+     */
     @Override
     public void deployAid() {
         System.out.println("Aid Ship "+ getName() + " is deploying " + getSuppliesOnBoard() + " of Medical aid.");
     }
 
+    /**
+     * returns a string detailing the attributes of the ship
+     * @return String, details of ship
+     */
     @Override
     public String toString() {
         return "AidShip [name='" + getName() +
@@ -53,6 +100,10 @@ public class AidShip extends Ship implements EmergencySupport{
                 ", hasHelipad=" + (hasHelipad ? "true" : "false") + "]";
     }
 
+    /**
+     * returns a string dictating the necessary attribute details for emergency readiness
+     * @return String, returns details related directly to aid/emergency assistance
+     */
     @Override
     public String getEmergencyReadinessReport() {
         return "Aid Ship Emergency Report:\n" +
@@ -63,6 +114,10 @@ public class AidShip extends Ship implements EmergencySupport{
                 "=======================================";
     }
 
+    /**
+     * sets the current speed of the aid ship
+     * @param speed, sets speed of the ship (double)
+     */
     @Override
     public void setCurrentSpeed(double speed) {
         super.setCurrentSpeed(speed);
